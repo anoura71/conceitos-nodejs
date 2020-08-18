7e675883196d97e9490367fb8c9c085b03de7432
Artur Teixeira <anoura@gmail.com>
2020-08-18 13:35:08 -0300

    Desafio: Conceitos do Node.js

--- src/app.js
+++ src/app.js
@@ -1,33 +1,95 @@
 const express = require("express");
 const cors = require("cors");
+const { v4: uuid } = require('uuid');
 
-// const { v4: uuid } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+
 const repositories = [];
 
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
+
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
+
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  // Procura pelo repositório com o id informado
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  // Não encontrou
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' });
+  }
+  // Monta o objeto para atualização
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+  // Atualiza o repositório na lista
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
+
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  // Procura pelo repositório com o id informado
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  // Não encontrou
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' });
+  }
+  // Remove o repositório da lista
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
+
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  // Procura pelo repositório com o id informado
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  // Não encontrou
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' });
+  }
+  // Incrementa os likes do repositório encontrado
+  let repository = repositories[repositoryIndex];
+  repository.likes += 1;
+
+  return response.json(repository);
 });
+
 
 module.exports = app;
